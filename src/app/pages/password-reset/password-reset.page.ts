ccb12255d07bbf763ab7d71d883b02428136b3bf
jworkman <jworkman@na.com>
2022-06-30 13:10:21 -0400

    auth crud operations

--- src/app/pages/password-reset/password-reset.page.ts
+++ src/app/pages/password-reset/password-reset.page.ts
@@ -32,7 +32,7 @@ export class PasswordResetPage implements OnInit {
       ]],
       password: ['', [Validators.required,
                       Validators.minLength(8), 
-                      // Validators.pattern("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$")
+                      Validators.pattern("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$")
       ]],
       confirmPassword: ['', [Validators.required,
       ]]
